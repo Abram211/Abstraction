43220c22a4ca67e4a50bc9508b64d4ef4c857caa
Abram <tacksontiger@gmail.com>
2025-03-19 00:09:48 +0300

    commit #2
    In the NV on line number 2, i declared an instance variable and on line number 4 to 7, generated a constructure.

---
+++ src/Dell.java
@@ -0,0 +1,8 @@
+public class Dell extends Computer{
+    int released;
+
+    public Dell(String core, int storage, int released) {
+        super(core, storage);
+        this.released = released;
+    }
+}
