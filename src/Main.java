0c252909396724265b190bbf7324b0bfc317f0e9
Abram <tacksontiger@gmail.com>
2025-03-19 14:11:33 +0300

    commit #1
    In the NV on line number 6 to 9, i called the method of Hp class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Computer hp = new Hp("i7",1,16);
         Computer dell = new Dell("i9",532,2024);
+
+        System.out.println("Displaying details of HP ");
+        hp.displayDetails();
+        System.out.println("Cost of hp "+ hp.computeValue(450,5)+"USD");
+        System.out.println();
     }
 }
